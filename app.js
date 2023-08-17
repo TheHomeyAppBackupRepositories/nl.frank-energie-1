855ad99a6d7d810b61f0b3ebe7876426383fec6e
Your Name <you@example.com>
2023-08-17 22:09:17 +0200

    Upgrade 1.0.11 to 1.0.12 jq app.json .

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MyApp = void 0;
+exports.FrankEnergieApp = void 0;
 const homey_1 = __importDefault(require("homey"));
 const PriceStore_1 = __importDefault(require("./lib/PriceStore"));
 const isBetween_1 = __importDefault(require("dayjs/plugin/isBetween"));
@@ -16,7 +16,7 @@ const PricePercentageThanDayAverage_1 = __importDefault(require("./lib/listeners
 const PriceAbsolute_1 = __importDefault(require("./lib/listeners/PriceAbsolute"));
 const PriceAbsoluteThanNextXHours_1 = __importDefault(require("./lib/listeners/PriceAbsoluteThanNextXHours"));
 const PriceAbsoluteBetweenHours_1 = __importDefault(require("./lib/listeners/PriceAbsoluteBetweenHours"));
-class MyApp extends homey_1.default.App {
+class FrankEnergieApp extends homey_1.default.App {
     constructor() {
         super(...arguments);
         this._listeners = [];
@@ -28,7 +28,7 @@ class MyApp extends homey_1.default.App {
         dayjs_1.default.extend(isBetween_1.default);
         dayjs_1.default.extend(utc_1.default);
         dayjs_1.default.extend(timezone_1.default);
-        PriceStore_1.default.initialize(this);
+        PriceStore_1.default.initialize();
         this._listeners = [
             new PriceChanged_1.default(this),
             new PriceAbsolute_1.default(this),
@@ -41,9 +41,16 @@ class MyApp extends homey_1.default.App {
         for (const listener of this._listeners) {
             await listener.initialize();
         }
+        PriceStore_1.default.registerApp(this);
         this.log('nl.frank-energie has been initialized');
         await PriceStore_1.default.requestUpdate();
     }
+    async onUninit() {
+        PriceStore_1.default.deregisterApp(this);
+    }
+    getListeners() {
+        return this._listeners;
+    }
 }
-exports.MyApp = MyApp;
-module.exports = MyApp;
+exports.FrankEnergieApp = FrankEnergieApp;
+module.exports = FrankEnergieApp;
